f298c1e752b9ecff89e6e4a3d11fd9a204edecae
jose vergara <joss1091@gmail.com>
2020-02-24 18:16:38 -0500

    actualizar api key
    se actualizan los datos de la app de google pra poner los propios

--- resources/lib/youtube_plugin/__init__.py
+++ resources/lib/youtube_plugin/__init__.py
@@ -10,40 +10,40 @@
 
 key_sets = {
     'youtube-tv': {
-        'id': 'ODYxNTU2NzA4NDU0LWQ2ZGxtM2xoMDVpZGQ4bnBlazE4azZiZThiYTNvYzY4',
-        'key': 'QUl6YVN5QzZmdlpTSkhBN1Z6NWo4akNpS1J0N3RVSU9xakUyTjNn',
-        'secret': 'U2JvVmhvRzlzMHJOYWZpeENTR0dLWEFU'
+        'id': 'Mzc3MjY4MTg4OTMyLWNtOHIwZm8wMnE1ODAzZG8zZnY1YWZjNTJrdWRmMmxp',
+        'key': 'QUl6YVN5Q0ZfdGFXeWNaNXhtWDdSY0tab05mR0h5LUN0bDMtTGZV',
+        'secret': 'aEdGQ1c1OUNwMVBLTjIyYmdBZ2g3YkY1'
     },
     'provided': {
         '0': {  # youtube-plugin-for-kodi-2
-            'id': 'NDMwNTcyNzE0ODgyLTlqcDdjYzVvMmZkZGdhZGM2bWM4anB1a2JmYjlsczdv',
-            'key': 'QUl6YVN5REIxVGU4MElUejdYR3ZXVGRzazhVMjVlVHY5WWlDS2Zz',
-            'secret': 'ekE5d2lqLWZsSXRZMzVrSHBsN3NmZWt4'
+            'id': 'Mzc3MjY4MTg4OTMyLWNtOHIwZm8wMnE1ODAzZG8zZnY1YWZjNTJrdWRmMmxp',
+            'key': 'QUl6YVN5Q0ZfdGFXeWNaNXhtWDdSY0tab05mR0h5LUN0bDMtTGZV',
+            'secret': 'aEdGQ1c1OUNwMVBLTjIyYmdBZ2g3YkY1'
         },
         '1': {  # youtube-plugin-for-kodi
-            'id': 'Mjk0ODk5MDY0NDg4LWE4a2MxazFqZDAwa2FtcXJlMHZkMm5mdHVpaWZyZjZh',
-            'key': 'QUl6YVN5Q1p3UXVvc0ZKYlF6bnFucXBxcFlsYUpXVk1uMTZ3QnZz',
-            'secret': 'S1RrQktJTk41dmY0T3dqMU5ZeVhMemJl',
+            'id': 'Mzc3MjY4MTg4OTMyLWNtOHIwZm8wMnE1ODAzZG8zZnY1YWZjNTJrdWRmMmxp',
+            'key': 'QUl6YVN5Q0ZfdGFXeWNaNXhtWDdSY0tab05mR0h5LUN0bDMtTGZV',
+            'secret': 'aEdGQ1c1OUNwMVBLTjIyYmdBZ2g3YkY1',
         },
         '2': {  # youtube-plugin-for-kodi-3
-            'id': 'NjM5NjIwMzY5MzYxLWJvYW9qNDM1dDJncDRybjNyNHFyNm52aW8yOXAwZnJi',
-            'key': 'QUl6YVN5QVA0eFJteU5KcndXcTFwNm5KUy1Wd25yY2dIWGxZT3Fr',
-            'secret': 'V1luZGxUQWxLaU1lZjhRT2N5UENkZ1dF'
+            'id': 'Mzc3MjY4MTg4OTMyLWNtOHIwZm8wMnE1ODAzZG8zZnY1YWZjNTJrdWRmMmxp',
+            'key': 'QUl6YVN5Q0ZfdGFXeWNaNXhtWDdSY0tab05mR0h5LUN0bDMtTGZV',
+            'secret': 'aEdGQ1c1OUNwMVBLTjIyYmdBZ2g3YkY1'
         },
         '3': {  # youtube-plugin-for-kodi-4
-            'id': 'NTMxNTEzODY4MDE5LWk1aGJoMDh1aWY4OWhrM2tzNnRmOGR1aTlndWl0dWVn',
-            'key': 'QUl6YVN5REJBRzB0ZXRrblVralo4b3QzRW95MEltUXFYNzRXWU1n',
-            'secret': 'VHpsZ2lkZXlNdmhCcFdiZTAyWi0xQzVy'
+            'id': 'Mzc3MjY4MTg4OTMyLWNtOHIwZm8wMnE1ODAzZG8zZnY1YWZjNTJrdWRmMmxp',
+            'key': 'QUl6YVN5Q0ZfdGFXeWNaNXhtWDdSY0tab05mR0h5LUN0bDMtTGZV',
+            'secret': 'aEdGQ1c1OUNwMVBLTjIyYmdBZ2g3YkY1'
         },
         '4': {  # youtube-plugin-for-kodi-5
-            'id': 'NzYxMjUyMTUxMDQ3LXN1ZGNpMjMyNzBhZjM2cWdpcDg0ZHYxaWM3dXZkYzhi',
-            'key': 'QUl6YVN5RFo1ZXk0T1JueGpHQTI1RkRCRGFteVVJakRTLTZKRUhR',
-            'secret': 'VzNad0psdko0U2VRaEkza0tqbFhBRFk3'
+            'id': 'Mzc3MjY4MTg4OTMyLWNtOHIwZm8wMnE1ODAzZG8zZnY1YWZjNTJrdWRmMmxp',
+            'key': 'QUl6YVN5Q0ZfdGFXeWNaNXhtWDdSY0tab05mR0h5LUN0bDMtTGZV',
+            'secret': 'aEdGQ1c1OUNwMVBLTjIyYmdBZ2g3YkY1'
         },
         '5': {  # youtube-plugin-for-kodi-6
-            'id': 'ODY1MTkxMjg0NTM0LXNzMDlvbzBhMDg1c3BmNHVvbnAzcmdmb2hqc2hzNGUy',
-            'key': 'QUl6YVN5QXc2VWtjREJpVk14ZXZxaGs3WE9la1BRU3dSaWNKaThR',
-            'secret': 'Q1IxOGd6VWlNTi1XRVNVdGc0dE1LNWdz'
+            'id': 'Mzc3MjY4MTg4OTMyLWNtOHIwZm8wMnE1ODAzZG8zZnY1YWZjNTJrdWRmMmxp',
+            'key': 'QUl6YVN5Q0ZfdGFXeWNaNXhtWDdSY0tab05mR0h5LUN0bDMtTGZV',
+            'secret': 'aEdGQ1c1OUNwMVBLTjIyYmdBZ2g3YkY1'
         }
     }
 }
